185e4f73a466b01e8ae063a5842c49d1bcb07e86
Santi <santioa30@hotmail.com>
2024-01-15 17:48:32 -0600

    Fix style for List Component

--- src/componets/Dashboard/ListCard.jsx
+++ src/componets/Dashboard/ListCard.jsx
@@ -51,7 +51,7 @@ const ListCard = ( { machines } ) => {
     } 
 
     return (
-        <div className='font-roboto 2xl:max--[100%] lg:max-w-[600px] lg:max-h-[350px] h-[350px] card__container block w-full pl-8 py-5 max-w-full max-h-80 2xl:max-h-96 bg-white rounded-2xl shadow-cardShadow '>
+        <div className='font-roboto 2xl:max--[100%] lg:max-w-[100%] lg:max-h-[350px] h-[350px] card__container block w-full pl-8 py-5 max-w-full max-h-80 2xl:max-h-96 bg-white rounded-2xl shadow-cardShadow '>
             <h3 className='font-roboto font-bold w-full text-2xl md:text-3xl xl:text-4xl text-grey-text ' >Maquinas</h3>
             <ul className='h-[80%] flex flex-col overflow-y-auto pr-5 sm:pr-[10%] mt-3' >
                 {
