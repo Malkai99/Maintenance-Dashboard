66ec3fdcda57ffa7bf6fa8a4a8dd8979f05b3906
Santi <santioa30@hotmail.com>
2024-02-02 01:08:00 -0600

    Add useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import './App.css'
 import DashboardContent from './componets/Dashboard/DashboardContent';
 import DashboardHeader from './componets/Dashboard/DashboardHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,15 +16,17 @@ function App() {
   }, []);
 
   return (
-    <div className={`App w-full h-screen flex flex-col bg-white-smoke ${isDarkMode ? 'theme-dark' : 'theme-light'}`}>
-      {/* <header className="App- relative bg-white-smoke">
-        <Header />
-      </header> */}
-      <section className=' block relative w-full h-full lg:px-20 md:px-10 px-5' >
-        <DashboardHeader />
-        <DashboardContent />
-      </section>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={`App w-full h-screen flex flex-col bg-white-smoke ${isDarkMode ? 'theme-dark' : 'theme-light'}`}>
+        {/* <header className="App- relative bg-white-smoke">
+          <Header />
+        </header> */}
+        <section className=' block relative w-full h-full lg:px-20 md:px-10 px-5' >
+          <DashboardHeader />
+          <DashboardContent />
+        </section>
+      </div>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/componets/hooks/useGetCells.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query"
+
+const getShifts = async() => {
+    const baseAPIUrl = import.meta.env.VITE_REACT_APP_API_URL
+    const response = await (await fetch(`${baseAPIUrl}/api/product/select/plc`))
+                            .then(response => response.json())
+
+    return response;
+}
+
+const useGetCells = () => {
+    const query = useQuery({
+        queryKey: ["shifts"],
+        queryFn: () => {
+         return getShifts();
+        },
+      });
+    return query;
+}
+
+export default useGetCells;
