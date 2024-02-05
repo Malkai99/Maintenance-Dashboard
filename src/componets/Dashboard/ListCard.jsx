0db240599200a868b0f896a536cdcf831a1a01cf
Santi <santioa30@hotmail.com>
2024-02-05 16:37:56 -0600

    Update component with Modal and Tooltip

--- src/componets/Dashboard/ListCard.jsx
+++ src/componets/Dashboard/ListCard.jsx
@@ -1,13 +1,12 @@
 import { useState, Fragment } from 'react'
 import Carousel from '../Utils/Carousel'
+import { FaInfoCircle } from 'react-icons/fa';
+import Modal from '../Utils/Modal';
+import Tooltip from '../Utils/Tooltip';
 
 const ListCard = ( { machines } ) => {
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedMachine, setSelectedMachine] = useState(null);
-    const styles = {
-        buttonStyles: 'focus:outline-0  text-blue-button border-blue-button hover:border-blue-button border bg-transparent border-opacity-40 hover:border-opacity-100 hover:bg-blue-button hover:bg-opacity-[.05]',
-        transitionStyle: 'transition-bg button-ease duration-200'
-    };
 
     const getColorStatus = (isActive) => {
         if(isActive) return 'text-green-success';
@@ -16,19 +15,15 @@ const ListCard = ( { machines } ) => {
 
     const getStatusDef = (isActive) => {
         const status ={
-            "true" : "Active",
-            "false": "Inactive"
+            "true" : "Activo",
+            "false": "Inactivo"
         }
         return status[isActive]
     };
 
     const handleMachineClick = (machine) => {
         setModalOpen(true);
-        if(machine.requests.length == 0 ){
-            setSelectedMachine(machine.requests[0]);
-        }else{
-            setSelectedMachine(machine.requests);
-        }
+        setSelectedMachine(machine.requests)
     };
     
     const closeModal = () => {
@@ -47,17 +42,26 @@ const ListCard = ( { machines } ) => {
                 </Fragment>
             )
         }))
-    } 
+    }
 
     return (
         <div className='font-roboto 2xl:max--[100%] lg:max-w-[100%] lg:max-h-[350px] h-[350px] card__container block w-full pl-8 py-5 max-w-full max-h-80 2xl:max-h-96 bg-white rounded-2xl shadow-cardShadow '>
-            <h3 className='font-roboto font-bold w-full text-2xl md:text-3xl xl:text-4xl text-grey-text ' >Maquinas</h3>
+            <h3 className='font-roboto font-bold w-full text-2xl md:text-3xl xl:text-4xl text-grey-text flex ' >
+                <p className='select-none' >Maquinas</p>
+                <Tooltip text={"Haz clic en la máquina 'Inactiva' para más detalles."}>
+                    <FaInfoCircle
+                    style={{ marginLeft: '5px', cursor: 'pointer', width: '20px' }}
+                    title="Más información"
+                    />
+                </Tooltip>
+                
+            </h3>
             <ul className='h-[80%] flex flex-col overflow-y-auto pr-5 sm:pr-[10%] mt-3' >
                 {
                     machines.map((machine,index) => {
                         return (
                             <li key={index} className='relative flex justify-between items-center flex-wrap h-auto w-full my-5'>
-                                <p>{machine.name}</p>
+                                <p className='cursor-default select-none' >{machine.name}</p>
                                 <p onClick={() => handleMachineClick(machine)} className={`${!machine.isActive ? 'cursor-pointer' : 'cursor-default' } select-none ${getColorStatus(machine.isActive)} `} >{getStatusDef(machine.isActive)}</p>
                             </li>
                         );
@@ -65,19 +69,13 @@ const ListCard = ( { machines } ) => {
                 }
             </ul>
             {modalOpen && selectedMachine && (
-                <div className='fixed inset-0 flex items-center justify-center z-10'>
-                <div className='absolute inset-0 bg-black  opacity-50' onClick={closeModal}></div>
-                <div className='z-20 bg-white p-8 w-[90%] max-w-[500px] rounded-lg shadow-cardShadow flex flex-col'>
-                    <h3 className='text-xl font-semibold mb-4'>Información de la máquina</h3>
+                <Modal title={"Información de la máquina"} closeModal={closeModal} >
                     <Carousel>
                         {getMachines()}
                     </Carousel>
-                    <button className={`mt-8 ${styles.buttonStyles} ${styles.transitionStyle}`} onClick={closeModal}>
-                        Cerrar
-                    </button>
-                </div>
-                </div>
+                </Modal>
             )}
+
         </div>
     )
 }
